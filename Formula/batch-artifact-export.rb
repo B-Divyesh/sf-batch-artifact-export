6be77789cc911bc81870d2b594a6f672ad78eef9
Param Factory <factory@sociobot.in>
2026-09-05 22:55:17 +0000

    chore: point package manifests at v0.1.1

--- Formula/batch-artifact-export.rb
+++ Formula/batch-artifact-export.rb
@@ -1,11 +1,11 @@
 class BatchArtifactExport < Formula
   desc "Deterministic batch exports through local converters"
   homepage "https://batch-artifact-export.sociobot.in"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
-  url "https://github.com/B-Divyesh/sf-batch-artifact-export/releases/download/v0.1.0/batch-artifact-export-macos-universal.tar.gz"
-  sha256 "4ddd7a69b32e2ec05b4f250a5890b7e71efe402df0e1ed02653ae915d8ddbe8a"
+  url "https://github.com/B-Divyesh/sf-batch-artifact-export/releases/download/v0.1.1/batch-artifact-export-macos-universal.tar.gz"
+  sha256 "f0b7d8f2eedb768f1ad2189a031390a88b1dfbbf47e8813a8566db6c80f72a05"
 
   def install
     bin.install "batch-artifact-export"
